3674473aeb202f486df25a1a13f5f676803f24d1
qzxst <1120398127@qq.com>
2024-11-16 05:02:28 +0800

    : Bumped to

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -3,8 +3,7 @@ param(
     [String]$upstream = "lrqzxst/my-win-apps:master"
 )
 
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
-https://gitee.com/lrqzxst/my-win-apps.git
