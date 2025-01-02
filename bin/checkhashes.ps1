58edfc9ec542f78409c588ffe496ccc997094beb
qzxst <1120398127@qq.com>
2025-01-03 01:35:48 +0800

    修改checkhashes.ps1

--- bin/checkhashes.ps1
+++ bin/checkhashes.ps1
@@ -2,8 +2,4 @@ if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkhashes = "$env:SCOOP_HOME/bin/checkhashes.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 
-Write-Host "checkhashes: $checkhashes"  -ForegroundColor Red
-Write-Host "dir: $dir" -ForegroundColor Red
-Write-Host "& '$checkhashes' -Dir '$dir' $($args | ForEach-Object { "$_ " })" -ForegroundColor Red
-# exit 0
 Invoke-Expression -Command "& '$checkhashes' -Dir '$dir' $($args | ForEach-Object { "$_ " })"
