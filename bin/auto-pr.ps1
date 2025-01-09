f01508a54ef62ec5fe5da5043b6c9a789b8b28a6
qzxst <1120398127@qq.com>
2025-01-09 23:11:05 +0800

    change something

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,8 +1,11 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "lrqzxst/my-win-apps:master"
+    [String]$upstream = "master"
 )
 
+$base = "lrqzxst/my-win-apps:"
+$upstream = $base + $upstream
+Write-Host "Upstream: $upstream"
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
