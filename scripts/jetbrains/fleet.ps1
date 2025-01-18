51dfd6ad13a18fc9eff811c41697f07a980eb490
qzxst <1120398127@qq.com>
2025-01-19 01:15:15 +0800

    change md

--- scripts/jetbrains/fleet.ps1
+++ scripts/jetbrains/fleet.ps1
@@ -4,14 +4,14 @@ if (!(Test-Path "$persist_dir\app\Fleet.cfg")) {
     info 'Copying Fleet.cfg file...'
 
     $current = (Split-Path $dir | Join-Path -ChildPath 'current') -replace '\\', '/'
+    $filePath = "$dir\app\Fleet.cfg"
+    info "filepath:$filePath"
 
-    $filePath = Get-Content "$dir\app\Fleet.cfg"
     $newLines = @(
         "java-options=-Dfleet.config.path=$current/profile/config",
         "java-options=-Dfleet.caches.path=$current/profile/caches",
         "java-options=-Dfleet.log.path=$current/profile/log"
     )
-    Add-Content -Path $filePath -Value $newLines
 
-    Set-Content -LiteralPath "$dir\app\Fleet.cfg" -Value $CONT -Encoding 'Ascii' -Force
+    Add-Content -Path $filePath -Value $newLines -Encoding 'Ascii' -Force
 }
